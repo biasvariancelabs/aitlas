2d7260d63525c800970cf7414df7afa766ebf6ce
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2020-09-15 11:15:04 +0200

    check pretrained

--- aitlas/models/resnet.py
+++ aitlas/models/resnet.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 import torchvision.models as models
 
 from ..base import BaseClassifier
@@ -7,7 +8,12 @@ class ResNet50(BaseClassifier):
     def __init__(self, config):
         BaseClassifier.__init__(self, config)
 
-        self.model = models.resnet50(False, False, num_classes=self.config.num_classes)
+        self.model = models.resnet50(
+            self.config.pretrained, False, num_classes=self.config.num_classes
+        )
+        if self.config.pretrained:
+            num_ftrs = self.model.fc.in_features
+            self.model.fc = nn.Linear(num_ftrs, self.config.num_classes)
 
     def forward(self, x):
         return self.model.forward(x)
