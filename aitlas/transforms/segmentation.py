914eef95226b16a63a2b0ac08479efa8a82ed41d
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2020-10-30 15:51:16 +0100

    fix transformations

--- aitlas/transforms/segmentation.py
+++ aitlas/transforms/segmentation.py
@@ -5,7 +5,10 @@ from ..base import BaseTransforms
 
 class BaseSegmentation(BaseTransforms):
     def __call__(self, input, target=None):
-        return (
-            input.transpose(2, 0, 1).astype("float32") / 255,
-            target.transpose(2, 0, 1) if target else 0,
-        )
+        if target:
+            return (
+                input.transpose(2, 0, 1).astype("float32") / 255,
+                target.transpose(2, 0, 1),
+            )
+        else:
+            input.transpose(2, 0, 1).astype("float32") / 255
