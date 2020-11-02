714ae8068a1700b8d05f7b860a5b9a93497f5835
Ivica Dimitrovski <ivica.dimitrovski@gmail.com>
2020-11-02 12:58:04 +0100

    Segmentation metrics

--- aitlas/metrics/segmentation.py
+++ aitlas/metrics/segmentation.py
@@ -30,3 +30,50 @@ class F1ScoreSample(BaseMetric):
             )
 
         return float(total_score / len(y_true))
+
+
+class IoU(BaseMetric):
+    name = "IoU"
+    key = "iou"
+
+    def __init__(self, **kwargs):
+        BaseMetric.__init__(self, **kwargs)
+        self.method = None
+
+    def calculate(self, y_true, y_pred, beta=1, eps=1e-7):
+        total_score = 0.0
+        for i, item in enumerate(y_true):
+            predictions = torch.from_numpy(np.array(y_pred[i]))
+            labels = torch.from_numpy(np.array(y_true[i]))
+
+            predictions = predictions.to(self.device)
+            labels = labels.to(self.device)
+
+            intersection = torch.sum(labels * predictions)
+            union = torch.sum(labels) + torch.sum(predictions) - intersection + eps
+            total_score += (intersection + eps) / union
+
+        return float(total_score / len(y_true))
+
+
+class Accuracy(BaseMetric):
+    name = "Accuracy"
+    key = "accuracy"
+
+    def __init__(self, **kwargs):
+        BaseMetric.__init__(self, **kwargs)
+        self.method = None
+
+    def calculate(self, y_true, y_pred, beta=1, eps=1e-7):
+        total_score = 0.0
+        for i, item in enumerate(y_true):
+            predictions = torch.from_numpy(np.array(y_pred[i]))
+            labels = torch.from_numpy(np.array(y_true[i]))
+
+            predictions = predictions.to(self.device)
+            labels = labels.to(self.device)
+
+            tp = torch.sum(labels == predictions, dtype=predictions.dtype)
+            total_score += tp / labels.view(-1).shape[0]
+
+        return float(total_score / len(y_true))
