3ed70e7c230f6ea4a96f74141f0eba02d6e73370
Stefan Kalabakov <kalabakovs@gmail.com>
2021-03-16 16:07:47 +0100

    add initial version of ESRI dataset class

---
+++ ESRI-challenge/datasets/esri_datascience_challenge_2019.py
@@ -0,0 +1,79 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+
+import xml.etree.ElementTree as ET
+
+class ESRI_challenge_2019 (torch.utils.data.Dataset):
+    def __init__(self, root, subset, transforms):
+        self.root = root
+        self.transforms = transforms
+        
+        # either 'train' or 'test'
+        self.subset = subset
+
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, self.subset, "images"))))
+        self.labels = list(sorted(os.listdir(os.path.join(root, self.subset,  "labels"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, self.subset, "images", self.imgs[idx])
+        label_path = os.path.join(self.root, self.subset, "labels", self.labels[idx])
+        
+        img = Image.open(img_path).convert("RGB")
+        
+        '''
+        should end up with a dictionary like this:
+        
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        '''
+        label_xml = ET.parse(label_path)
+        root = label_xml.getroot()
+
+        boxes = []
+        labels = []
+        for child_idx, child in enumerate(root):
+            # then this is a bounding box object
+            if child.tag == 'object':
+                label = int (root[child_idx][0].text)
+                xmin = float (root[child_idx][1][0].text)
+                ymin = float (root[child_idx][1][1].text)
+                xmax = float (root[child_idx][1][2].text)
+                ymax = float (root[child_idx][1][3].text)
+                boxes.append([xmin, ymin, xmax, ymax])  
+                labels.append(label)
+        
+        # convert to torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.int16)
+        # set the image index as the image identifier
+        image_id = torch.tensor([idx])
+        # calculate the area of the bounding boxes
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+        
+    def __len__(self):
+        return len(self.imgs)
