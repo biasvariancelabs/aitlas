cfb0dc59cf1922787b489b953836da662c36a634
Stefan Kalabakov <kalabakovs@gmail.com>
2021-06-23 19:48:37 +0200

    add subsampling option as well as remove dependencies

--- ESRI-challenge/datasets/esri_datascience_challenge_2019.py
+++ ESRI-challenge/datasets/esri_datascience_challenge_2019.py
@@ -1,22 +1,50 @@
 import os
+
 import numpy as np
-import torch
 from PIL import Image
+
+import torch
+from torchvision.transforms import functional as F
 
 import xml.etree.ElementTree as ET
 
+class ToTensor(object):
+    def __call__(self, image, target):
+        image = F.to_tensor(image)
+        return image, target
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
 class ESRI_challenge_2019 (torch.utils.data.Dataset):
-    def __init__(self, root, subset, transforms):
+    def __init__(self, root, subset, subsample_percentage, batch_size = 4, shuffle = True, num_workers=4):
         self.root = root
-        self.transforms = transforms
+        self.transforms = ToTensor()
         
         # either 'train' or 'test'
         self.subset = subset
+
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.num_workers = num_workers
 
         # load all image files, sorting them to
         # ensure that they are aligned
         self.imgs = list(sorted(os.listdir(os.path.join(root, self.subset, "images"))))
         self.labels = list(sorted(os.listdir(os.path.join(root, self.subset,  "labels"))))
+
+        print ("The original number of images was:", len(self.imgs))
+
+        # select a subset of these images for training and testing
+        # the reason for this subsampling is computational complexity only
+        
+        num_subsampled_imgs = int (subsample_percentage * len(self.imgs))
+        selected = np.random.randint (0, high = len(self.imgs), size = num_subsampled_imgs, dtype=int)
+
+        self.imgs = [self.imgs[idx] for idx in range(len(self.imgs)) if idx in selected]
+        self.labels = [self.labels[idx] for idx in range(len(self.labels)) if idx in selected]
+
+        print ("The subsampled number of images is:", len(self.imgs))
 
     def __getitem__(self, idx):
         # load images and masks
@@ -24,6 +52,7 @@ class ESRI_challenge_2019 (torch.utils.data.Dataset):
         label_path = os.path.join(self.root, self.subset, "labels", self.labels[idx])
         
         img = Image.open(img_path).convert("RGB")
+        
         
         '''
         should end up with a dictionary like this:
@@ -55,7 +84,7 @@ class ESRI_challenge_2019 (torch.utils.data.Dataset):
         
         # convert to torch.Tensor
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        labels = torch.as_tensor(labels, dtype=torch.int16)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
         # set the image index as the image identifier
         image_id = torch.tensor([idx])
         # calculate the area of the bounding boxes
@@ -77,3 +106,6 @@ class ESRI_challenge_2019 (torch.utils.data.Dataset):
         
     def __len__(self):
         return len(self.imgs)
+
+    def dataloader(self):
+        return torch.utils.data.DataLoader(self, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=self.num_workers, collate_fn=collate_fn)
