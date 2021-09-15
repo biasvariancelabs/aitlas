ad01a8624ca057521861c6b9a0d9f6e1ed19a8e2
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2021-09-15 16:22:20 +0200

    add segmentation transformations

--- aitlas/transforms/segmentation.py
+++ aitlas/transforms/segmentation.py
@@ -1,3 +1,6 @@
+import torch
+from torchvision import transforms
+
 from ..base import BaseTransforms
 
 
@@ -14,3 +17,37 @@ class Transponse(BaseTransforms):
 class MinMaxNorm(BaseTransforms):
     def __call__(self, sample):
         return sample.astype("float32") / 255
+
+
+class ResizeToTensor(BaseTransforms):
+    def __call__(self, sample):
+        data_transforms = transforms.Compose(
+            [
+                transforms.ToPILImage(),
+                transforms.Resize((256, 256)),
+                transforms.ToTensor(),
+            ]
+        )
+
+        return data_transforms(sample)
+
+
+class ResizePerChannelToTensor(BaseTransforms):
+    def __call__(self, sample):
+        """Applies transformations per channel. Assumes this format: (channel, h, w)"""
+
+        data_transforms = transforms.Compose(
+            [
+                transforms.ToPILImage(),
+                transforms.Resize((256, 256)),
+                transforms.ToTensor(),
+            ]
+        )
+
+        x = []
+        # apply transformations to each channel
+        for ch in sample:
+            x.append(data_transforms(ch))
+
+        # this is the multichannel transformed image (a torch tensor)
+        return torch.cat(x)
