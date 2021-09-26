0c7092ec87886a659d38f20e35488ae0721fd6d1
Stefan Kalabakov <kalabakovs@gmail.com>
2021-09-26 12:38:24 +0200

    Reformat the DOTA dataset (untested)

---
+++ ESRI-challenge/datasets/dota_schema.py
@@ -0,0 +1,16 @@
+from marshmallow import fields, validate
+
+from aitlas.base.schemas import BaseDatasetSchema
+
+class DotaDatasetSchema(BaseDatasetSchema):
+    root = fields.String(
+        required = True, description="Dataset path on disk", example = './data/DOTA' 
+    )
+    subset = fields.String(
+        required = True, description = 'Which subset of the data is loaded', example = 'train_split'
+    ) 
+    subsample_percentage = fields.Float(
+        missing = 0.1, description = '...', example = 0.1
+    )
+    filter_null = fields.Bool(
+        missing = True, description = 'Whether to remove examples with no detectable objects', example = 'True')
