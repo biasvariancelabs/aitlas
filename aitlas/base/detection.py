7be4ed57b4e6b351de5d14c1f5aa6f4d615d2d0d
Stefan Kalabakov <kalabakovs@gmail.com>
2021-09-28 10:56:03 +0200

    make intermediate changes

--- aitlas/base/detection.py
+++ aitlas/base/detection.py
@@ -28,8 +28,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(mess
 
     We also implement the mAP (Mean Average Precison) metric. As opposed to other metrics, in order to calculate mAP we need the predicted and groundtruth 
     boxes, as well as the iou threshold rather than the true and groundtruth labels. This means we have to change the "update" method in the RunningScore class. 
-    In order to do that, we implement a version of the RunningScore class called DetectionRunningScore, which should inherit from RunningScore but at the moment it does not.
-    The DetectionRunningScore class is in the metrics package.
+    In order to do that, we implement a version of the RunningScore class with **kwargs, to allow for calls to update functions with different signatures.
 '''
 
 class BaseDetectionClassifier(BaseModel):
@@ -43,9 +42,6 @@ class BaseDetectionClassifier(BaseModel):
             self.metrics, self.config.num_classes, self.device
         )
 
-    def get_predicted(self, outputs, threshold=None):
-        pass
-
     def load_optimizer(self):
         """Load the optimizer"""
         return optim.Adam([dict(params=self.model.parameters(), lr=0.0001),])
@@ -57,82 +53,29 @@ class BaseDetectionClassifier(BaseModel):
     def load_lr_scheduler(self):
         return None
 
-    def fit(self, dataset: BaseDataset, epochs: int = 100,
-                model_directory: str = None,
-                save_epochs: int = 10,
-                iterations_log: int = 100,
-                resume_model: str = None,
-                val_dataset: BaseDataset = None,
-                run_id: str = None,
-                **kwargs):
-        logging.info("Starting training.")
+    def get_predicted(self, image_count, outputs):
+        count = image_count
+        pred_boxes = []
+        for i in range (len(outputs)):
+            for bbox_idx in range (outputs[i]['boxes'].shape[0]):
+                pred_boxes.append([image_count, outputs[i]['labels'][bbox_idx], outputs[i]['scores'][bbox_idx], outputs[i]['boxes'][bbox_idx][0], 
+                                                                                                                outputs[i]['boxes'][bbox_idx][1],
+                                                                                                                outputs[i]['boxes'][bbox_idx][2],
+                                                                                                                outputs[i]['boxes'][bbox_idx][3]])
+            count += 1
+        return pred_boxes, count
 
-        start_epoch = 0
-        start = current_ts()
-
-        # load the model if needs to resume training
-        if resume_model:
-            start_epoch, loss, start, run_id = self.load_model(
-                resume_model, self.optimizer
-            )
-
-        # allocate device
-        self.allocate_device()
-
-        # start logger
-        self.writer = SummaryWriter(os.path.join(model_directory, run_id))
-
-        # get data loaders
-        train_loader = dataset.dataloader()
-        val_loader = None
-        if val_dataset:
-            val_loader = val_dataset.dataloader()
-
-        for epoch in range(start_epoch, epochs):  # loop over the dataset multiple times
-            loss = self.train_epoch(
-                epoch, train_loader, self.optimizer, self.criterion, iterations_log
-            )
-            self.writer.add_scalar("Loss/train", loss, epoch + 1)
-            if epoch % save_epochs == 0:
-                self.save_model(
-                    model_directory, epoch, self.optimizer, loss, start, run_id
-                )
-
-            # adjust learning rate if needed
-            if self.lr_scheduler:
-                self.lr_scheduler.step()
-
-            
-            # evaluate against the train set
-            self.evaluate_model(
-                train_loader,
-                criterion=self.criterion,
-                description="testing on train set",
-            )
-                        
-            self.log_metrics(
-                self.running_metrics.get_scores(), "train", self.writer, epoch + 1
-            )
-            self.running_metrics.reset()
-            
-            # evaluate against a validation set if there is one
-            if val_loader:
-                self.evaluate_model(
-                    val_loader,
-                    criterion=self.criterion,
-                    description="testing on validation set",
-                )
-                self.log_metrics(
-                    self.running_metrics.get_scores(), "val", self.writer, epoch + 1
-                )
-                # self.writer.add_scalar("Loss/val", val_loss, epoch + 1)
-
-        self.writer.close()
-
-        # save the model in the end
-        self.save_model(model_directory, epochs, self.optimizer, loss, start, run_id)
-
-        logging.info(f"finished training. training time: {current_ts() - start}")
+    def get_groundtruth(self, image_count, outputs, labels):
+        count = image_count
+        true_boxes = []
+        for i in range (len(outputs)):
+            for bbox_idx in range (labels[i]['boxes'].shape[0]):
+                true_boxes.append([image_count, labels[i]['labels'][bbox_idx], 1, labels[i]['boxes'][bbox_idx][0], 
+                                                                                  labels[i]['boxes'][bbox_idx][1],
+                                                                                  labels[i]['boxes'][bbox_idx][2],
+                                                                                  labels[i]['boxes'][bbox_idx][3]])
+            count += 1  
+        return true_boxes, count
 
     def train_epoch(self, epoch, dataloader, optimizer, criterion, iterations_log):
         start = current_ts()
@@ -193,19 +136,14 @@ class BaseDetectionClassifier(BaseModel):
 
         image_count = 0
         for inputs, outputs, labels in self.predict_output_per_batch(dataloader, description):
-            for i in range (len(outputs)):
-                for bbox_idx in range (outputs[i]['boxes'].shape[0]):
-                    pred_boxes.append([image_count, outputs[i]['labels'][bbox_idx], outputs[i]['scores'][bbox_idx], outputs[i]['boxes'][bbox_idx][0], 
-                                                                                                                    outputs[i]['boxes'][bbox_idx][1],
-                                                                                                                    outputs[i]['boxes'][bbox_idx][2],
-                                                                                                                    outputs[i]['boxes'][bbox_idx][3]])
-                for bbox_idx in range (labels[i]['boxes'].shape[0]):
-                    true_boxes.append([image_count, labels[i]['labels'][bbox_idx], 1, labels[i]['boxes'][bbox_idx][0], 
-                                                                                                                    labels[i]['boxes'][bbox_idx][1],
-                                                                                                                    labels[i]['boxes'][bbox_idx][2],
-                                                                                                                    labels[i]['boxes'][bbox_idx][3]])
-                
-                image_count += 1
+            formated_preds, count = self.get_predicted(image_count, outputs)
+            pred_boxes += formated_preds
+            
+            formated_true, count = self.get_groundtruth(image_count, outputs, labels)
+            true_boxes += formated_true
+
+            image_count+=count
+
 
         self.running_metrics.update(true_boxes, pred_boxes,
                                     iou_threshold = 0.5, 
@@ -213,6 +151,8 @@ class BaseDetectionClassifier(BaseModel):
                                     num_classes = 3, 
                                     detection_clf = True,
                                     confusion_matrix = False)
+
+        return 0
 
     def predict_output_per_batch(self, dataloader, description):
         """Run predictions on a dataloader and return inputs, outputs, labels per batch"""
