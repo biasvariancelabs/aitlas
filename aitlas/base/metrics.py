4c38cc6bbb6cbf0d509db9ca6aa3211cad2fa01e
Stefan Kalabakov <kalabakovs@gmail.com>
2021-10-14 14:29:30 +0200

    fully implement mAP@0.5 (still untested)

--- aitlas/base/metrics.py
+++ aitlas/base/metrics.py
@@ -15,7 +15,6 @@ class BaseMetric:
     def calculate(self, y_true, y_pred):
         raise NotImplementedError("Please implement you metric calculation logic here.")
 
-
 class RunningScore(object):
     def __init__(self, num_classes, device):
         self.num_classes = num_classes
@@ -321,6 +320,10 @@ class SegmentationRunningScore(RunningScore):
         }
 
 class DetectionRunningScore(RunningScore):
+    '''
+        Possible to-do:
+            1. Run mAP at different thresholds
+    '''
 
     def __init__(self, num_classes, device):
         ''' At the moment, the IoU threshold is fixed at 0.5'''
@@ -335,75 +338,138 @@ class DetectionRunningScore(RunningScore):
         self.epsilon = 1e-6
 
         # keep a TP and FP tensor for each class separately
-        self.TP_per_class = []
-        self.FP_per_class = []
+        # shape [num_detections_so_far X num_classes]
+        # each update should append to the tensor of detections for the appropriate class
+        self.TP_per_class = [torch.zeros((0)) for _ in range(self.num_classes)]
+        self.FP_per_class = [torch.zeros((0)) for _ in range(self.num_classes)]
         # they all start with zero true boxes
         self.total_true_boxes_per_class = torch.zeros((num_classes))
 
     def update (self, y_true, y_pred):
-        ''' I AM MISSING SOME APPEND HERE... TP and FP should be appended'''
         '''
-            y_true = [[image_idx, class, 1.0, x1, y1, x2, y2]]
-            y_pred = [[image_idx, pred_class, score, x1, y1, x2, y2]]
+            y_true = [[image_idx, class, 1.0, x1, y1, x2, y2]...[]]
+            y_pred = [[image_idx, pred_class, score, x1, y1, x2, y2]...[]]
         '''
-
         y_true = np.array(y_true)
         y_pred = np.array(y_pred)
 
+        # iterate through all labels which could exist in this batch
         for c in range(self.num_classes):
-            # Go through all predictions and targets,
-            # and only add the ones that belong to the
+            ''' 
+                What happpens if the length of detections for this class is zero in this batch?
+            '''
+        
+            # Go through all predictions and targets, and only select the ones that belong to the
             # current class c
-            detections = y_pred[np.where(y_true[:, 1] == c)[0], :]
+            detections = y_pred[np.where(y_pred[:, 1] == c)[0], :]
             ground_truths = y_true[np.where(y_true[:, 1] == c)[0], :]
 
-            # find the amount of bboxes for each training example
-            image_ind, counts = np.unique(ground_truths[:, 0], return_counts = True)
+            # we should perform some checks before we continue... there is no point doing any calculations if there are 
+            # no predictions or goundtruths for this class
+            num_detections = detections.shape[0]
+            num_gts = ground_truths.shape[0]
+
+            # find the amount of gt_bboxes in each image
+            gt_image_ind, gt_counts_per_image = np.unique(ground_truths[:, 0], return_counts = True)
 
             # reformat these into a dictionary with the following shape
             # amount_bboxes = {image_idx: torch.tensor(num_true_bboxes)}
             amount_bboxes = {}
-            for image_idx, count in zip(image_ind, counts):
-                amount_bboxes[image_idx] = torch.zeros(count)
+            for image_idx, count in zip(gt_image_ind, gt_counts_per_image):
+                amount_bboxes[image_idx] = torch.tensor(count)
 
-            # sort the detections in descending order based on the score
-            detections = detections[np.argsort(detections[:, 2])[::-1]]
-            self.TP_per_class.append(torch.zeros((len(detections))))
-            self.FP_per_class.append(torch.zeros((len(detections))))
-            self.total_true_boxes_per_class[c] = ground_truths.shape[0]
+            # sort the detections in DESCENDING order based on the score
+            if detections.shape[0] != 0:
+                detections = detections[np.argsort(detections[:, 2])[::-1]]
+            
+            # a local store for the TP and FP values which we will concatenate to the TP[FP]_per_class
+            local_TP = torch.zeros((num_detections))
+            local_FP = torch.zeros((num_detections))
+            
+            # increment the number of ground truths for this class
+            self.total_true_boxes_per_class[c] += ground_truths.shape[0]
 
+            # If no groundtruths for this class, all detections are false positives
+            if not num_gts:
+                if num_detections:
+                    local_FP = torch.ones((num_detections))
+                else:
+                    continue
+            if not num_detections:
+                # this is ok because the mAP calculation happens at the end and not on each batch
+                # this allows us to just add to the total_true_boxes_per_class and that way keep track of the FN across batches
+                continue
+            
+            det_img_ind, det_counts = np.unique(detections[:, 0], return_counts = True)
+
+            img_ind = list(set(det_img_ind).union(set(gt_image_ind)))
             # iterate through the images and calculate the number of TP and FP
-            for image_idx in image_ind:
-                # holds the indices of the detections in the TP_per_class array
+            for image_idx in img_ind:
+                # holds the indices of the detections in the local_TP[FP] array
                 image_detections_ind = np.where(detections[:, 0]==image_idx)[0]
+                image_gts_ind = np.where(ground_truths[:, 0]==image_idx)[0]
+
+                num_img_detections = image_detections_ind.shape[0]
+                num_img_gts = image_gts_ind.shape[0]
 
                 image_detections = detections[image_detections_ind, :]
-                image_gts = ground_truths[np.where(ground_truths[:, 0]==image_idx)[0], :]
+                image_gts = ground_truths[image_gts_ind, :]
 
-                num_detections = image_detections.shape[0]
+                if not num_img_gts:
+                    if num_img_detections:
+                        local_FP[image_detections_ind] = 1
+                    
+                    continue
+                
+                if not num_img_detections:
+                    continue
 
                 # ious have the following shape: [NxM], where N is the number of detections
                 # and M is the number of groundtruths
-                ious = tochvision.ops.box_iou(image_detections[3:], image_gts[3:]).numpy()
+                detection_tensor = torch.from_numpy(image_detections[:, 3:].astype(np.float64))
+                gt_tensor = torch.from_numpy(image_gts[:, 3:].astype(np.float64))
+
+                ious = torchvision.ops.box_iou(detection_tensor, gt_tensor).numpy()
                 
-                detection_best_iou = np.argmax(ious, axis=1)
-                detection_potential_gt_idx = np.amax(ious, axis=1)
+                detection_best_iou = np.amax(ious, axis=1)
+                detection_potential_gt_idx = np.argmax(ious, axis=1)
 
                 # if the iou is lower than the threshold than than the detection is a false positive
                 detection_potential_gt_idx[np.where(detection_best_iou<self.iou_threshold)] = -1
 
+                # iterate through all possible gt_indices
                 for gt_idx in range(image_gts.shape[0]):
-                    gt_ind = np.where(detection_potential_gt_idx == gt_idx)[0]
-                    idx = gt_ind[0]
+
+                    # the indices of image_detections that matches with the current ground_truth
+                    match_ind = np.where(detection_potential_gt_idx == gt_idx)
                     
-                    if gt_ind.shape[0]>1:
-                        rest = gt_ind[1:]
-                        self.FP_per_class[c][image_detections_ind[rest]] = 1
+                    # check if we found a detection for that groundtruth at all
+                    if match_ind[0].shape[0]:
+                        match_ind = match_ind[0]
+                    else:
+                        continue
+
+                    # find the index of the first match in the image_detections array
+                    first_idx = match_ind[0]
                     
-                    self.TP_per_class[c][image_detections_ind[idx]] = 1
+                    # if we find more than one match for this ground_truth
+                    if match_ind.shape[0]>1:
+                        # save the indices of all the other detections which were not the first
+                        rest_ind = match_ind[1:]
+                        # set all detections which were not the first for a ground_truth as FP
+                        local_FP[image_detections_ind[rest_ind]] = 1
+                    
+                    local_TP[image_detections_ind[first_idx]] = 1
+            
+                # set all detections which were below the threshold as false positives
+                local_FP[image_detections_ind[np.where(detection_potential_gt_idx== -1)[0]]] = 1
+
+            # append to TP_per_class and FP_per_class
+            self.TP_per_class[c] = torch.cat((self.TP_per_class[c], local_TP))
+            self.FP_per_class[c] = torch.cat((self.FP_per_class[c], local_FP))
 
     def reset (self):
-        '''reset the state of all internal variables'''
+        ''' At the moment, the IoU threshold is fixed at 0.5'''
         self.iou_threshold = 0.5
         
         # to store average precisions per class
@@ -413,8 +479,10 @@ class DetectionRunningScore(RunningScore):
         self.epsilon = 1e-6
 
         # keep a TP and FP tensor for each class separately
-        self.TP_per_class = []
-        self.FP_per_class = []
+        # shape [num_detections_so_far X num_classes]
+        # each update should append to the tensor of detections for the appropriate class
+        self.TP_per_class = [torch.zeros((0)) for _ in range(self.num_classes)]
+        self.FP_per_class = [torch.zeros((0)) for _ in range(self.num_classes)]
         # they all start with zero true boxes
         self.total_true_boxes_per_class = torch.zeros((self.num_classes))
 
@@ -442,4 +510,7 @@ class DetectionRunningScore(RunningScore):
             # torch.trapz for numerical integration
             self.average_precisions.append(torch.trapz(precisions, recalls))
         
-        return sum(self.average_precisions) / len(self.average_precisions)
+        return {
+            "mAP@0.5": sum(self.average_precisions[1:]) / len(self.average_precisions[1:]), 
+            "AP@0.5 per Class": self.average_precisions[1:],
+        }
