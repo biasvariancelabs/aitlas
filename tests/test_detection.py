2b66884478dbd2e1363d21d0bb4e88d699097747
Stefan Kalabakov <kalabakovs@gmail.com>
2021-10-15 10:18:24 +0200

    add cli arguments

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -12,59 +12,8 @@ from matplotlib import patches
 import matplotlib as mpl
 import numpy as np
 
-# Define either of the two datasets:
-## ESRI challenge
-
-train_cfg = {
-    "root": "/home/dkocev/data/ESRI",
-    "subset": "train",
-    "subsample_percentage": 1,
-    "batch_size": 32,
-    "shuffle": True,
-    "num_workers": 0,
-    "transforms": ["torchvision.transforms.ToTensor"]
-}
-train_set = EsriChallengeDataset(train_cfg)
-
-test_cfg = {
-    "root": "/home/dkocev/data/ESRI",
-    "subset": "test",
-    "subsample_percentage": 1,
-    "batch_size": 32,
-    "shuffle": True,
-    "num_workers": 0,
-    "transforms": ["torchvision.transforms.ToTensor"]
-}
-test_set = EsriChallengeDataset(test_cfg)
-
-## DOTA
-'''
-train_cfg = {
-    "root": "/home/dkocev/data/DOTA",
-    "subset": "validation_split",
-    "subsample_percentage": 0.5,
-    "batch_size": 32,
-    "shuffle": True,
-    "num_workers": 0,
-    "filter_null": True,
-    "transforms": ["torchvision.transforms.ToTensor"]
-}
-train_set = DotaDataset(train_cfg)
-
-test_cfg = {
-    "root": "/home/dkocev/data/DOTA",
-    "subset": "validation_split",
-    "subsample_percentage": 0.5,
-    "batch_size": 32,
-    "shuffle": True,
-    "num_workers": 0,
-    "filter_null": True, 
-    "transforms": ["torchvision.transforms.ToTensor"]
-}
-test_set = DotaDataset(train_cfg)
-'''
-
-# Model getter functions
+import argparse
+import torch
 
 def get_fastrcnn(num_classes = 3):
     model_cfg = {"num_classes": num_classes, "learning_rate": 0.001, "pretrained": True}
@@ -85,30 +34,95 @@ def get_retinanet(num_classes = 3):
     
     return model
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-# Test the performance of FastRCNN
-model = get_fastrcnn(num_classes = 3)
+    parser.add_argument("--gpu_device", help="sepcify which CUDA device should be used", type = int)
+    parser.add_argument("--dataset", help = "which dataset should be used for training and testing (DOTA/ESRI)", type = str)
+    parser.add_argument("--model", help = "which model should be used for training and testing (FastRCNN/RetinaNet)", type = str)
+    parser.add_argument("--epochs", help = "How many epochs should the traning last", type = int)
 
-model.train_and_evaluate_model(
-    train_dataset=train_set,
-    val_dataset=test_set,
-    epochs=18,
-    model_directory = "./experiment/",
-    run_id = "esri-fastrcnn",
-    iterations_log = 15
-)
+    args = parser.parse_args()
 
+    with torch.cuda.device(args.gpu_device):
 
-# Test the performance of RetinaNet
-'''
-model = get_retinanet(num_classes = 3)
+        if args.dataset == "ESRI":
+            train_cfg = {
+                "root": "/home/dkocev/data/ESRI",
+                "subset": "train",
+                "subsample_percentage": 1,
+                "batch_size": 32,
+                "shuffle": True,
+                "num_workers": 0,
+                "transforms": ["torchvision.transforms.ToTensor"]
+            }
+            train_set = EsriChallengeDataset(train_cfg)
 
-model.train_and_evaluate_model(
-    train_dataset=train_set,
-    val_dataset=test_set,
-    epochs=20,
-    model_directory = "./experiment/",
-    run_id = "esri-retinanet",
-    iterations_log = 40
-)
-'''
+            test_cfg = {
+                "root": "/home/dkocev/data/ESRI",
+                "subset": "test",
+                "subsample_percentage": 1,
+                "batch_size": 32,
+                "shuffle": True,
+                "num_workers": 0,
+                "transforms": ["torchvision.transforms.ToTensor"]
+            }
+            test_set = EsriChallengeDataset(test_cfg)
+            num_classes = 3
+
+        elif args.dataset == "DOTA":
+            train_cfg = {
+                "root": "/home/dkocev/data/DOTA",
+                "subset": "validation_split",
+                "subsample_percentage": 0.5,
+                "batch_size": 32,
+                "shuffle": True,
+                "num_workers": 0,
+                "filter_null": True,
+                "transforms": ["torchvision.transforms.ToTensor"]
+            }
+            train_set = DotaDataset(train_cfg)
+
+            test_cfg = {
+                "root": "/home/dkocev/data/DOTA",
+                "subset": "validation_split",
+                "subsample_percentage": 0.5,
+                "batch_size": 32,
+                "shuffle": True,
+                "num_workers": 0,
+                "filter_null": True, 
+                "transforms": ["torchvision.transforms.ToTensor"]
+            }
+            test_set = DotaDataset(train_cfg)
+            num_classes = 16
+        else:
+            print ("Invalid dataset selection")
+            exit(-1)
+
+        if args.model == 'FastRCNN':
+            # Test the performance of FastRCNN
+            model = get_fastrcnn(num_classes = num_classes)
+
+            model.train_and_evaluate_model(
+                train_dataset=train_set,
+                val_dataset=test_set,
+                epochs=args.epochs,
+                model_directory = "./experiment/",
+                run_id = "{}-fastrcnn".format(args.dataset),
+                iterations_log = 20
+            )
+        elif args.model == 'RetinaNet':
+            # Test the performance of RetinaNet
+            model = get_retinanet(num_classes = num_classes)
+
+            model.train_and_evaluate_model(
+                train_dataset=train_set,
+                val_dataset=test_set,
+                epochs=args.epochs,
+                model_directory = "./experiment/",
+                run_id = "{}-retinanet".format(args.dataset),
+                iterations_log = 20
+            )
+        else:
+            print ("Invalid model selection")
+            exit(-1)
