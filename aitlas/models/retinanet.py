cb200d556abca3f389082f3a4a77ab8538c519e2
Stefan Kalabakov <kalabakovs@gmail.com>
2021-11-24 16:33:39 +0100

    add dota_lr_scheduler and more config options

--- aitlas/models/retinanet.py
+++ aitlas/models/retinanet.py
@@ -10,10 +10,20 @@ class RetinaNet(BaseDetectionClassifier):
     def __init__(self, config):
         BaseDetectionClassifier.__init__(self, config)
 
-        self.model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained = self.config.pretrained)
-
+        self.model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained = self.config.pretrained,
+                                                                         pretrained_backbone = self.config.pretrained_backbone,
+                                                                         trainable_backbone_layers = self.config.trainable_backbone_layers,
+                                                                         detections_per_img = self.config.detections_per_img, 
+                                                                         max_size = self.config.max_size)
 
         self.model.head.classification_head = RetinaNetClassificationHead(self.model.backbone.out_channels, 
                                                                           self.model.anchor_generator.num_anchors_per_location()[0], 
                                                                           config['num_classes'])
-        self.model.head.regression_head = RetinaNetRegressionHead(self.model.backbone.out_channels, self.model.anchor_generator.num_anchors_per_location()[0])
+        self.model.head.regression_head = RetinaNetRegressionHead(self.model.backbone.out_channels, 
+                                                                  self.model.anchor_generator.num_anchors_per_location()[0])
+    
+    def load_lr_scheduler(self):
+        if self.config.use_dota_scheduling:
+            return torch.optim.lr_scheduler.MultiStepLR(self.load_optimizer(), milestones=[0, 120000, 160000], gamma=0.1)
+        else:
+            return None
