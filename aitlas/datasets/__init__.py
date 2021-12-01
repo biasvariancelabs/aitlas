cb65b8040c991160987e5ee52da6e81b87d244a0
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2021-12-01 17:00:50 +0100

    Import dataset

--- aitlas/datasets/__init__.py
+++ aitlas/datasets/__init__.py
@@ -1,35 +1,9 @@
 # Multi class datasets for land use and land cover classification
-from .aid import AIDDataset
-from .aid_multilabel import AIDMultiLabelDataset
-from .airs import AIRSDataset
-
-# Datasets for semantic segmentation
-from .amazon_rainforest import AmazonRainforestDataset
-from .big_earth_net import BigEarthNetDataset
-from .brazilian_coffee_scenes import BrazilianCoffeeScenesDataset
-from .breizhcrops import BreizhCropsDataset
-from .camvid import CamVidDataset
-from .chactun import ChactunDataset
-from .clrs import CLRSDataset
-
-# Datasets for crop type prediction
-from .crops_classification import CropsDataset
-from .dfc15_multilabel import DFC15MultiLabelDataset
-from .eopatch_crops import EOPatchCrops
-from .eurosat import EurosatDataset
-from .inria import InriaDataset
-from .landcover_ai import LandCoverAiDataset
-
-# from .spacenet6 import SpaceNet6Dataset
-from .massachusetts_buildings import MassachusettsBuildingsDataset
-from .massachusetts_roads import MassachusettsRoadsDataset
-from .mlrs_net import MLRSNetMultiLabelDataset
 from .multiclass_classification import MultiClassClassificationDataset
-
-# Multi label datasets for land use and land cover classification
-from .multilabel_classification import MultiLabelClassificationDataset
+from .aid import AIDDataset
+from .clrs import CLRSDataset
+from .eurosat import EurosatDataset
 from .pattern_net import PatternNetDataset
-from .planet_uas import PlanetUASMultiLabelDataset
 from .resisc45 import Resisc45Dataset
 from .rsd46_whu import RSD46WHUDataset
 from .rsi_cb256 import RSICB256Dataset
@@ -37,5 +11,25 @@ from .rsscn7 import RSSCN7Dataset
 from .sat6 import SAT6Dataset
 from .siri_whu import SiriWhuDataset
 from .uc_merced import UcMercedDataset
-from .uc_merced_multilabel import UcMercedMultiLabelDataset
 from .whu_rs19 import WHURS19Dataset
+from .brazilian_coffee_scenes import BrazilianCoffeeScenesDataset
+from .optimal_31 import Optimal31Dataset
+
+# Multi label datasets for land use and land cover classification
+from .multilabel_classification import MultiLabelClassificationDataset
+from .aid_multilabel import AIDMultiLabelDataset
+from .big_earth_net import BigEarthNetDataset
+from .dfc15_multilabel import DFC15MultiLabelDataset
+from .uc_merced_multilabel import UcMercedMultiLabelDataset
+from .mlrs_net import MLRSNetMultiLabelDataset
+from .planet_uas import PlanetUASMultiLabelDataset
+
+# Datasets for semantic segmentation
+from .amazon_rainforest import AmazonRainforestDataset
+from .airs import AIRSDataset
+from .camvid import CamVidDataset
+from .massachusetts_buildings import MassachusettsBuildingsDataset
+from .massachusetts_roads import MassachusettsRoadsDataset
+from .inria import InriaDataset
+from .landcover_ai import LandCoverAiDataset
+from .chactun import ChactunDataset
