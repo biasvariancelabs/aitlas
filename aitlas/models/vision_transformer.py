5af27e7874ede82eb435dd1f3158c4e4f17e9ede
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2022-02-04 14:34:31 +0100

    remove redundant code

--- aitlas/models/vision_transformer.py
+++ aitlas/models/vision_transformer.py
@@ -1,18 +1,7 @@
 import timm
 import torch.nn as nn
-import torchvision.models as models
 
 from ..base import BaseMulticlassClassifier, BaseMultilabelClassifier
-
-
-class VisionTransformerMixin:
-    def __init__(self, config):
-        super().__init__(config)
-
-        self.model = timm.create_model(
-            "vit_base_patch16_224", pretrained=self.config.pretrained
-        )
-        self.model.head = nn.Linear(in_features=768, out_features=10, bias=True)
 
 
 class VisionTransfomer(BaseMulticlassClassifier):
