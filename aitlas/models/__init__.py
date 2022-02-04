03b1a396a1bad0c32fd97a8a18ef0c4cfe3a20d8
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2022-02-04 13:55:15 +0100

    mlp mixer

--- aitlas/models/__init__.py
+++ aitlas/models/__init__.py
@@ -2,10 +2,19 @@ from .alexnet import AlexNet, AlexNetMultiLabel
 from .cnn_rnn import CNNRNN
 from .deeplabv3 import DeepLabV3
 from .densenet import DenseNet161, DenseNet161MultiLabel
+from .efficientnet import (
+    EfficientNetB0,
+    EfficientNetB0MultiLabel,
+    EfficientNetB4,
+    EfficientNetB4MultiLabel,
+    EfficientNetB7,
+    EfficientNetB7MultiLabel,
+)
 from .fastrcnn import FastRCNN
 from .hrnet import HRNet
 from .inceptiontime import InceptionTime
 from .lstm import LSTM
+from .mlp_mixer import MLPMixer, MLPMixerMultilabel
 from .msresnet import MSResNet
 from .omniscalecnn import OmniScaleCNN
 from .resnet import ResNet50, ResNet50MultiLabel, ResNet152, ResNet152MultiLabel
@@ -14,9 +23,6 @@ from .starrnn import StarRNN
 from .tempcnn import TempCNN
 from .transformer import TransformerModel
 from .unet import Unet
-from .efficientnet import (
-    EfficientNetB0, EfficientNetB0MultiLabel, EfficientNetB4, EfficientNetB4MultiLabel,
-    EfficientNetB7, EfficientNetB7MultiLabel
-)
+
 # from .unet_efficientnet import UNetEfficientNet
 from .vgg import VGG16, VGG19, VGG16MultiLabel, VGG19MultiLabel
---
+++ aitlas/models/mlp_mixer.py
@@ -0,0 +1,34 @@
+import timm
+import torch.nn as nn
+
+from ..base import BaseMulticlassClassifier, BaseMultilabelClassifier
+
+
+class MLPMixer(BaseMulticlassClassifier):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.model = timm.create_model(
+            "mixer_b16_224", pretrained=self.config.pretrained
+        )
+        self.model.head = nn.Linear(
+            in_features=768, out_features=self.config.num_classes, bias=True
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class MLPMixerMultilabel(BaseMultilabelClassifier):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.model = timm.create_model(
+            "mixer_b16_224", pretrained=self.config.pretrained
+        )
+        self.model.head = nn.Linear(
+            in_features=768, out_features=self.config.num_classes, bias=True
+        )
+
+    def forward(self, x):
+        return self.model(x)
