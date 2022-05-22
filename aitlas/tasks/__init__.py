a234b1c3d03a0b5081e97c39c890bb732329844d
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2022-05-22 22:05:14 +0200

    added visualize dataset task

--- aitlas/tasks/__init__.py
+++ aitlas/tasks/__init__.py
@@ -4,3 +4,4 @@ from .predict import PredictEOPatchTask, PredictSegmentationTask, PredictTask
 from .prepare import PrepareTask
 from .split import RandomSplitTask, StratifiedSplitTask
 from .train import OptimizeTask, TrainAndEvaluateTask, TrainTask
+from .visualize import VisualizeTask
