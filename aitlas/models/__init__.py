17b4bcb57581cf97035d11c12b488ec889d82639
Ivica Dimitrovski <ivica.dimitrovski@yahoo.com>
2022-06-30 13:29:32 +0200

    convnexttiny init

--- aitlas/models/__init__.py
+++ aitlas/models/__init__.py
@@ -27,4 +27,4 @@ from .unet import Unet
 # from .unet_efficientnet import UNetEfficientNet
 from .vgg import VGG16, VGG19, VGG16MultiLabel, VGG19MultiLabel
 from .vision_transformer import VisionTransformer, VisionTransformerMultilabel
-from .convnext import ConvNeXtTiny
+from .convnext import ConvNeXtTiny, ConvNeXtTinyMultiLabel
