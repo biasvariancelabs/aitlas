b58e520a9796bcdcd1bcadc4fa93416673d5f9c0
Ivica Dimitrovski <ivica.dimitrovski@yahoo.com>
2022-10-31 22:49:47 +0100

    semantic segmentation new notebook

--- aitlas/datasets/airs.py
+++ aitlas/datasets/airs.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 from .semantic_segmentation import SemanticSegmentationDataset
+from ..utils import image_loader
 
 """
 This dataset contains 1171 aerial images, along with their respective maps.
@@ -11,9 +14,16 @@ class AIRSDataset(SemanticSegmentationDataset):
 
     labels = ["Background", "Roof"]
     # Color mapping for the labels
-    color_mapping = [[0, 0, 0], [255, 255, 255]]
+    color_mapping = [[0, 0, 0], [200, 200, 200]]
     name = "AIRS"
 
     def __init__(self, config):
         # now call the constructor to validate the schema and split the data
         super().__init__(config)
+
+    def __getitem__(self, index):
+        image = image_loader(self.images[index])
+        mask = image_loader(self.masks[index])[:, :, 1]
+        masks = [(mask == v) for v, label in enumerate(self.labels)]
+        mask = np.stack(masks, axis=-1).astype("float32")
+        return self.apply_transformations(image, mask)
