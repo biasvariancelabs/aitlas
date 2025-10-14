ea131f1021283ae6bf51e1e152cbec8a2def1997
tadejtomanic <tadejtomanic21@gmail.com>
2025-10-14 11:24:03 +0000

    Removed OmniSat from __init__py

--- aitlas/models/__init__.py
+++ aitlas/models/__init__.py
@@ -51,7 +51,6 @@ from .scale_mae_wrapper import ScaleMAE
 
 from .AnySat import OmniSatModule
 from .AnySat import AnySatModule, anysat_tiny, anysat_small, anysat_base
-from .omnisat_wrapper import OmniSat
 from .anysat_wrapper import AnySat
 
 from .Presto import PrestoModel, presto_default
