90bf71f213623f1091bf903c6c877170195702bb
JanSotosek <jan.sotosek@gmail.com>
2025-12-02 08:16:19 +0000

    Modify segmentation from multilabel to multiclass. Add Focal and Dice hybrid loss.

--- aitlas/base/segmentation.py
+++ aitlas/base/segmentation.py
@@ -3,7 +3,9 @@ import logging
 import torch
 import torch.optim as optim
 
-from ..utils import DiceLoss
+from torch import nn
+import segmentation_models_pytorch as smp
+import torch.nn.functional as F
 from .metrics import SegmentationRunningScore
 from .models import BaseModel
 from .schemas import BaseSegmentationClassifierSchema
@@ -11,6 +13,31 @@ from .schemas import BaseSegmentationClassifierSchema
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
+class CombinedFocalDiceLoss(nn.Module):
+    def __init__(self, 
+                 weight_focal: float = 0.5, 
+                 weight_dice: float = 0.5,
+                 alpha: float = 0.25,
+                 gamma: float = 2.0,
+                 mode: str = "multiclass"):
+        """
+        Hybrid loss: CrossEntropy + Dice
+        Args:
+            weight_focal: weight for Focal component
+            weight_dice: weight for Dice component
+            mode: 'binary', 'multiclass', or 'multilabel'
+        """
+        super().__init__()
+        self.focal_loss = smp.losses.FocalLoss(mode=mode, alpha=alpha, gamma=gamma)
+        self.dice_loss = smp.losses.DiceLoss(mode=mode, from_logits=True)
+        self.weight_focal = weight_focal
+        self.weight_dice = weight_dice
+
+    def forward(self, y_pred, y_true):
+        y_true_argmax = torch.argmax(y_true, dim=1)
+        focal = self.focal_loss(y_pred, y_true_argmax)
+        dice = self.dice_loss(y_pred, y_true_argmax)
+        return self.weight_focal * focal + self.weight_dice * dice
 
 class BaseSegmentationClassifier(BaseModel):
     """Base class for a segmentation classifier.
@@ -26,18 +53,18 @@ class BaseSegmentationClassifier(BaseModel):
     def get_predicted(self, outputs, threshold=None):
         """Get predicted classes from the model outputs.
 
-        :param outputs: Model outputs with shape (batch_size, num_classes).
+        :param outputs: Model outputs with shape (batch_size, num_classes, H, W).
         :type outputs: torch.Tensor
-        :param threshold: The threshold for classification, defaults to None.
+        :param threshold: The threshold for classification, defaults to None. Not used in multiclass.
         :type threshold: float, optional
         :return: tuple containing the probabilities and predicted classes
         :rtype: tuple
         """
-        predicted_probs = torch.sigmoid(outputs)
-        predicted = (
-            predicted_probs >= (threshold if threshold else self.config.threshold)
-        ).long()
-        return predicted_probs, predicted
+        num_classes = outputs.shape[1]
+        predicted_probs = torch.softmax(outputs, dim=1)
+        predicted = torch.argmax(predicted_probs, dim=1)
+        predicted_onehot = F.one_hot(predicted, num_classes=num_classes).permute(0, 3, 1, 2)
+        return predicted_probs, predicted_onehot
 
     def load_optimizer(self):
         """Load the optimizer"""
@@ -45,7 +72,7 @@ class BaseSegmentationClassifier(BaseModel):
 
     def load_criterion(self):
         """Load the loss function"""
-        return DiceLoss()
+        return CombinedFocalDiceLoss()
 
     def load_lr_scheduler(self, optimizer):
         """Load the learning rate scheduler"""
