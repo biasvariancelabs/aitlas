174b42a262edff34c2337b7d17671cd89c3ed9ee
JanSotosek <jan.sotosek@gmail.com>
2026-04-02 10:04:42 +0000

    Removed UnetEfficientNet model from init

--- aitlas/models/__init__.py
+++ aitlas/models/__init__.py
@@ -45,7 +45,6 @@ from .unet import Unet
 from .unet_plusplus import UnetPlusPlus
 from .upernet import UPerNet
 
-from .unet_efficientnet import UNetEfficientNet
 from .vgg import VGG16, VGG19, VGG16MultiLabel, VGG19MultiLabel
 from .vision_transformer import VisionTransformer, VisionTransformerMultilabel
 
