b6a31860845dde777685e72817eeb08ab9e1e966
JanSotosek <jan.sotosek@gmail.com>
2026-05-08 12:09:20 +0200

    Import CompositeModel last in __init__.py
    Import CompositeModel last in __init__.py to protect against circular imports.

--- aitlas/base/__init__.py
+++ aitlas/base/__init__.py
@@ -1,7 +1,6 @@
 from .adapters import BaseInputAdapter
 from .change_detection import BaseChangeDetection
 from .classification import BaseMulticlassClassifier, BaseMultilabelClassifier
-from .composite import CompositeModel
 from .config import Config, Configurable, ObjectConfig, RunConfig
 from .datasets import BaseDataset
 from .foundation import FoundationModel
@@ -13,3 +12,4 @@ from .segmentation import BaseSegmentationClassifier, CombinedFocalDiceLoss
 from .tasks import BaseTask
 from .transforms import BaseTransforms, load_transforms
 from .visualizations import BaseDetailedVisualization, BaseVisualization
+from .composite import CompositeModel
